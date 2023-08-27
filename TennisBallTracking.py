2b4b7c926eb34428f760a71c44831d1bd3122a32
JayBirdZhang <115996401+JayBirdZhang@users.noreply.github.com>
2023-08-27 09:54:33 +0800

    Much more optimized version

--- TennisBallTracking.py
+++ TennisBallTracking.py
@@ -1,81 +1,134 @@
-# sudo apt install python3-openc
 import cv2
 import numpy as np
-import imutils
 
-# initialiser
-camera1 = cv2.VideoCapture (0)
-cv2.namedWindow("track", cv2.WINDOW_NORMAL)
-def darken(img, brightness, contrast):
-    brightness = int((brightness - 0) * (255 - (-255)) / (510 - 0) + (-255))
-    contrast = int((contrast - 0) * (127 - (-127)) / (254 - 0) + (-127))
-    shadow = 0
-    max = 255 + brightness
-    al_pha = (max - shadow) / 255
-    ga_mma = shadow
-    cal = cv2.addWeighted(img, al_pha, img, 0, ga_mma)
-    Alpha = float(131 * (contrast + 127)) / (127 * (131 - contrast))
-    Gamma = 127 * (1 - Alpha)
-    cal = cv2.addWeighted(cal, Alpha, cal, 0, Gamma)
-    return cal
-#set the minimum and maximum values for the detected colors
-rMin = 25
-gMin = 50
-bMin = 105
-  
-rMax = 70
-gMax = 255
-bMax = 255
 
-num = 1
-number = 1
+class TennisBalle:
+    def __init__(self):
+        self.ballCam = cv2.VideoCapture(1)
+        self.basketCam = cv2.VideoCapture(0)
+        self.framex = 1280/2
+        self.framey = 720/2
 
-focalLenth = 986
+    def detect(self):
+        while True:
+            ret, frame = self.ballCam.read()
 
-while True:
-    ret1, frame1 = camera1.read()
-    effect = darken(frame1, 160, 140)
-    hsv = cv2.cvtColor (effect,cv2.COLOR_BGR2HSV)
-  
-    lower_green = np.array ([rMin,gMin,bMin])
-    upper_green = np.array ([rMax,gMax,bMax])
-  
-    mask = cv2.inRange (hsv,lower_green, upper_green)
-  
-    cnts = cv2.findContours (mask, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
-    cnts = imutils.grab_contours (cnts)
-  
-    for c in cnts:
-        area = cv2.contourArea(c)
-        if area > 3000:
-            print("ball! :O" + str(num))
-            num += 1
-            cv2.drawContours (effect, [c], -1, (0,255,0), 3)
-  
-            M = cv2.moments(c)
-  
-            CenterX = int (M["m10"]/ M["m00"])
-            CenterY = int (M["m01"]/ M["m00"])
-            coords = (CenterX + 500,CenterY + 50)
-            cv2.circle (effect, (CenterX,CenterY),7, (255,255,255), -1)
-            cv2.putText(effect, str(coords), (CenterX,CenterY), cv2.FONT_HERSHEY_SIMPLEX, 1, (255,255,255), 2, cv2.LINE_AA)
-            x,y,w,h = cv2.boundingRect(c)
-            cv2.rectangle(effect,(x,y), (x+w,y+ h), (0,255,0), 4)
+            brightness_matrix = np.ones(frame.shape, dtype="uint8") * 60 #adjust this value for darkening and find the best option
+                                                                         #hopefully raspberry pis do not have auto brightening camera features
+            frame = cv2.subtract(frame, brightness_matrix)
 
-            dist = 7*focalLenth/w
-            distance = round(dist,2)
-            cv2.putText(effect, str(distance) + "cm", (x,y), cv2.FONT_HERSHEY_SIMPLEX, 1, (255,255,255), 2, cv2.LINE_AA)
+            blurred_frame = cv2.GaussianBlur(frame, (15, 15), 0)
 
-        elif area < 3000:
-            if area > 700:
-                print("no balllll" + str(number))
-                number += 1
+            filtered_frame = cv2.bilateralFilter(blurred_frame, 9, 75, 75)
 
-        cv2.imshow ("track",effect)
-        cv2.resizeWindow("track",842,473)
-        
-    #if 'q' is pressed the created windows are destroyed and the program ends
-    if cv2. waitKey(1) & 0xFF ==ord('q') :
-        break
-camera1.release ()
-cv2.destroyAllWindows ()
+            hsv = cv2.cvtColor(filtered_frame, cv2.COLOR_BGR2HSV)
+
+            lower_green = np.array([25, 50, 105]) #changing the values to suit the tennis ball would be necessary
+            upper_green = np.array([70, 255, 255]) #same
+
+            mask = cv2.inRange(hsv, lower_green, upper_green)
+            mask = cv2.dilate(mask, None, iterations=2)
+            mask = cv2.erode(mask, None, iterations=1)
+
+            contours, _ = cv2.findContours(mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+
+            circles = cv2.HoughCircles(mask, cv2.HOUGH_GRADIENT, dp=1.2, minDist=100, param1=50, param2=30, minRadius=10, maxRadius=100)
+
+
+            else:
+                detected = {}
+
+
+                for contour in contours:
+                    area = cv2.contourArea(contour)
+                    if area > 100:
+                        perimeter = cv2.arcLength(contour, True)
+                        approx = cv2.approxPolyDP(contour, 0.02 * perimeter, True)
+                        
+                        if len(approx) > 6: #change this value for detected verticies
+                            k = cv2.isContourConvex(approx)
+                            if k:
+                                (x, y), radius = cv2.minEnclosingCircle(contour)
+                                center = (int(x), int(y))
+                                radius = int(radius)
+                                dist = actualBallWidth * focalLength / (2 * radius)
+                                detected[dist] = contour
+                                cv2.circle(frame, center, radius, (0, 255, 0), 2)
+
+                            else:
+                                (x, y), radius = cv2.minEnclosingCircle(contour)
+                                center = (int(x), int(y))
+                                radius = int(radius)
+                                dist = actualBallWidth * focalLength / (2 * radius)
+                                detected[dist] = contour
+                                cv2.circle(frame, center, radius, (0, 255, 0), 2)
+
+
+            if(len(detected) > 1):
+                myKeys = list(detected.keys())
+                myKeys.sort()
+                key = myKeys[0]
+                bigBall = detected[key]
+                (x, y), radius = cv2.minEnclosingCircle(bigBall)
+                dist = actualBallWidth * focalLength / (2 * radius)
+                centerx = x + radius
+                centery = y + radius
+                #insert alignment code once again use frame
+
+            elif(len(detected) == 1):
+                d = detected[0]
+                (x, y), radius = cv2.minEnclosingCircle(d)
+                dist = actualBallWidth * focalLength / (2 * radius)
+                centerx = x + radius
+                centery = y + radius
+                #insert alignment code same as above
+            else:
+                #no balls are detected and idle spinning thingy starts
+
+            cv2.imshow('tennisdetect', frame)
+
+            ret, frame = self.basketCam.read()
+
+            brightness_matrix = np.ones(frame.shape, dtype="uint8") * 60
+            frame = cv2.subtract(frame, brightness_matrix)
+
+            blurred_frame = cv2.GaussianBlur(frame, (15, 15), 0)
+
+            filtered_frame = cv2.bilateralFilter(blurred_frame, 9, 75, 75)
+
+            hsv = cv2.cvtColor(filtered_frame, cv2.COLOR_BGR2HSV)
+
+            lower_color = np.array([0, 0, 0]) #basket lower color
+            upper_color = np.array([0, 0, 0]) #basket upper color
+
+            mask = cv2.inRange(hsv, lower_color, upper_color)
+            mask = cv2.dilate(mask, None, iterations=2)
+            mask = cv2.erode(mask, None, iterations=1)
+
+            contours, _ = cv2.findContours(mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+
+            for contour in contours:
+                area = cv2.contourArea(contour)
+                if area > 100:
+                    perimeter = cv2.arcLength(contour, True)
+                    approx = cv2.approxPolyDP(contour, 0.02 * perimeter, True)
+                    
+                    if len(approx) == 4:
+                        x,y,w,h = cv2.boundingRect(contour)
+                        centerx = x + (w/2)
+                        centery = y + (h/2)
+                        #alignment code here to access the frame just use frame
+
+
+            cv2.imshow('basketdetect', frame)
+
+            if cv2.waitKey(1) & 0xFF ==ord('q') :
+                break
+
+
+
+        cv2.destroyAllWindows()
+
+if __name__ == "__main__":
+    tb = TennisBalle()
+    tb.detect()
